6f6c6d48519d552eafc3b78eac31217238a4b5aa
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-02-01 22:06:41 +0300

    Create npc3.py

---
+++ npc3.py
@@ -0,0 +1,80 @@
+import file_operations
+import random
+import os
+from faker import Faker
+
+
+def fonts_swap_to_runic():
+    for fonts in skills:
+        for old, new in letter_mapping.items():
+            fonts = fonts.replace(old, new)
+        runic_skills.append(fonts)
+
+
+def cards_create_save_to_folder():
+    for y in range(10):
+        fake = Faker("ru_RU")
+        skill = random.sample(runic_skills, 3)
+        context = {
+            "first_name": fake.first_name_male(),
+            "last_name": fake.last_name_male(),
+            "town": fake.city(),
+            "job": fake.job(),
+
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+
+            "skill_1": skill[0],
+            "skill_2": skill[1],
+            "skill_3": skill[2]
+        }
+        with open(f'cards/charsheet_{y}.svg', 'w'):
+            file_operations.render_template(
+                "charsheet.svg", f"cards/charsheet_{y}.svg", context)
+
+
+def main():
+    os.makedirs("cards/", exist_ok=True)
+    fonts_swap_to_runic()
+    cards_create_save_to_folder()
+
+
+if __name__ == '__main__':
+
+    skills = [
+        "Стремительный прыжок", "Электрический выстрел",
+        "Ледяной удар", "Стремительный удар",
+        "Кислотный взгляд", "Тайный побег",
+        "Ледяной выстрел", "Огненный заряд"
+    ]
+
+    letter_mapping = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    runic_skills = []
+    main()
